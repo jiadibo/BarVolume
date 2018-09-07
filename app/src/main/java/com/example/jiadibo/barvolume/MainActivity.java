68308da65c9c6577cb5ab54ad654fcc27ae5bb96
jiadibo <jihad.strange@gmail.com>
2018-09-07 18:01:04 +0800

    finish

--- app/src/main/java/com/example/jiadibo/barvolume/MainActivity.java
+++ app/src/main/java/com/example/jiadibo/barvolume/MainActivity.java
@@ -2,12 +2,73 @@ package com.example.jiadibo.barvolume;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText edtLength, edtWidth, edtHeight;
+    private Button btnCalculate;
+    private TextView tvResult;
+    private static final String STATE_HASIL = "state_hasil";
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+
+        outState.putString(STATE_HASIL, tvResult.getText().toString());
+        super.onSaveInstanceState(outState);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtWidth = (EditText)findViewById(R.id.edt_width);
+        edtHeight = (EditText)findViewById(R.id.edt_height);
+        edtLength = (EditText)findViewById(R.id.edt_length);
+        btnCalculate = (Button)findViewById(R.id.btn_calculate);
+        tvResult = (TextView)findViewById(R.id.tv_result);
+
+        btnCalculate.setOnClickListener(this);
+
+        if (savedInstanceState != null){
+            String hasil = savedInstanceState.getString(STATE_HASIL);
+            tvResult.setText(hasil);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_calculate){
+            String lenght = edtLength.getText().toString().trim();
+            String width = edtWidth.getText().toString().trim();
+            String height = edtHeight.getText().toString().trim();
+            boolean isEmptyFields = false;
+            if (TextUtils.isEmpty(width)){
+                isEmptyFields = true;
+                edtWidth.setError("Width Harus di isi");
+            }
+            if (TextUtils.isEmpty(lenght)){
+                isEmptyFields = true;
+                edtLength.setError("Width Harus di isi");
+            }
+            if (TextUtils.isEmpty(height)){
+                isEmptyFields = true;
+                edtHeight.setError("Width Harus di isi");
+            }
+            if (!isEmptyFields){
+                double l = Double.parseDouble(lenght);
+                double w = Double.parseDouble(width);
+                double h = Double.parseDouble(height);
+                double volume = l*w*h;
+
+                tvResult.setText(String.valueOf(volume));
+
+            }
+        }
     }
 }
